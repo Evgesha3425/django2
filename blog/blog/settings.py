0c6b685c99d967e5dcf418e7c19e68b8c0281acc
Yauheni Eidelman <yauheni.skills@gmail.com>
2022-02-04 21:46:56 +0300

    Add last version of django2

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
     'posts',
+    'profiles',
 ]
 
 MIDDLEWARE = [
@@ -75,11 +77,16 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   "default": {
+       "ENGINE": "django.db.backends.postgresql",
+       "NAME": "django",
+       "USER": "django",
+       "PASSWORD": "django",
+       "HOST": "localhost",
+       "PORT": 5432,
+   }
 }
+
 
 
 # Password validation
--- blog/posts/admin.py
+++ blog/posts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from posts.models import Post, Tag
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("author", "title", "slug", "created_at")
+    fields = ("author", "title", "image", "slug", "text", "created_at")
+    readonly_fields = ("created_at",)
+    search_fields = ("title", "slug", "text")
+
+
+@admin.register(Tag)
+class TagsAdmin(admin.ModelAdmin):
+    list_display = ("title",)
+    fields = ("title",)
+    search_fields = ("title",)
