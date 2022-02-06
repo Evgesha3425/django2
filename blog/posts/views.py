91df97fd020aec66be45eff23eb55e398534095a
Yauheni Eidelman <yauheni.skills@gmail.com>
2022-02-06 15:53:51 +0300

    Add cw and hw #15

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,5 +1,20 @@
+import logging
+
+from django.contrib.auth.models import User
 from django.http import HttpResponse
+
+from posts.models import Post
+
+logger = logging.getLogger(__name__)
 
 
 def posts_index(request):
-   return HttpResponse("Posts index view")
+    result = ""
+    for post in Post.objects.filter(title="Test2"):
+        result += f"<div style='border: 1px solid black'>"
+        result += f"<h1>{post.title}</h1>"
+        result += f"<div>{post.text}</div>"
+        result += f"</div>"
+    return HttpResponse(result)
+
+
--- blog/profiles/models.py
+++ blog/profiles/models.py
@@ -1,3 +1,14 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="profiles"
+    )
+    age = models.IntegerField()
+    image = models.ImageField(blank=True, null=True)
+    status = models.TextField()
+    created_at = models.DateTimeField(
+        auto_now_add=True, db_index=True
+    )
